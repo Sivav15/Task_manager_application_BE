337b3fc65f1475078943919fa0a5685cc6cec888
Sivav15 <sivanathanv1999@gmail.com>
2024-07-19 13:52:40 +0530

    add router

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require("express");
-
 const dotenv = require("dotenv");
+const cors = require("cors");
 const database = require("./src/config/database");
-
+const authRoute = require("./src/routes/authRoute");
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 database();
+
+app.use("/api/auth", authRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
