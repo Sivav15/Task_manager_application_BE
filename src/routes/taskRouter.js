36047b7f8c7a887e8db1cea34a34e2617cae3e56
Sivav15 <sivanathanv1999@gmail.com>
2024-07-20 18:13:46 +0530

    validator update in task feature

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -1,15 +1,24 @@
 const express = require("express");
-const createTask = require("../controllers/task/createTask");
-const updateTask = require("../controllers/task/updateTask");
-const deleteTask = require("../controllers/task/deleteTask");
-const viewTasksByUser = require("../controllers/task/viewTasksByUser");
+const createTaskController = require("../controllers/task/createTask");
+const updateTaskController = require("../controllers/task/updateTask");
+const deleteTaskController = require("../controllers/task/deleteTask");
+const viewTasksByUserController = require("../controllers/task/viewTasksByUser");
 const validate = require("../middlewares/validator/validate");
+const createTaskSchema = require("../middlewares/validator/task/createTask");
+const viewTasksByUserSchema = require("../middlewares/validator/task/viewTasksByUser");
+const updateTaskSchema = require("../middlewares/validator/task/updateTask");
+const deleteTaskSchema = require("../middlewares/validator/task/deleteTask");
 
 const router = express.Router();
 
-router.get("/user/:user_id", validate, viewTasksByUser);
-router.post("/create", validate, createTask);
-router.put("/update/:id", validate, updateTask);
-router.delete("/delete/:id", validate, deleteTask);
+router.get(
+  "/user/:user_id",
+  viewTasksByUserSchema,
+  validate,
+  viewTasksByUserController
+);
+router.post("/create", createTaskSchema, validate, createTaskController);
+router.put("/update/:id", updateTaskSchema, validate, updateTaskController);
+router.delete("/delete/:id", deleteTaskSchema, validate, deleteTaskController);
 
 module.exports = router;
