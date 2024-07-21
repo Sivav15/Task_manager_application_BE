4b8c6f756f61bd3d5a92b4cc5fa6841642128ae1
Sivav15 <sivanathanv1999@gmail.com>
2024-07-21 09:48:54 +0530

    update in authenticztion moddleware.

--- src/middlewares/authenticateJWT.js
+++ src/middlewares/authenticateJWT.js
@@ -7,12 +7,12 @@ const authenticateJWT = (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "No token provided" });
+    return res.status(401).json({ message: "Access Token Required" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(403).json({ message: "Forbidden" });
+      return res.status(401).json({ message: "Access Token Expired" });
     }
     req.user = decoded;
     next();
